52ff69b5e3c6ac07c015c86720094ff0d9944497
Snig10dha j <av.sc.u4cse23113@av.students.amrita.edu>
2024-04-02 16:42:04 +0000

    myvariable.py is created

---
+++ vari.py
@@ -0,0 +1,11 @@
+# this file explains variables in python
+
+print ('My Blog is AIFOSS')
+
+print ('2+3')
+
+# + addition
+# - Subtraction
+# /(float division) 
+# //(integer division)
+# * multiply
